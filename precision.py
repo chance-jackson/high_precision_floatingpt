d8518d3918c3360454b08062b4a61f467b5eaed6
ninoco <neoneill@syr.edu>
2025-09-23 13:52:57 -0400

    addding fucntion

--- precision.py
+++ precision.py
@@ -1,6 +1,7 @@
 class Precision():
     def __init__(self, num):
         self.num = num
+
     def splitting(self):
         num_str = str(self.num)
         parts = num_str.split('.')
@@ -10,3 +11,7 @@ class Precision():
         else:
             dec = 0
         return(numer, dec)
+    
+    def __add__(self, other):
+        a_num, a_dec = self.splitting()
+        b_num, b_dec = other.splitting()
