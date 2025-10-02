e4ae514c388ea9e7c581b47abf51c4af21521d04
Chance Jackson <cjacks34@syr.edu>
2025-10-02 18:21:53 +0000

    simple loops to compute execution time

--- sorting.py
+++ sorting.py
@@ -1,5 +1,8 @@
 from precision import Precision as p
 from numpy.random import randint
+import time
+import matplotlib.pyplot as plt
+import numpy as np
 
 def bubble_sort(arr: list) -> list:
 
@@ -17,11 +20,16 @@ def bubble_sort(arr: list) -> list:
 
     return arr                                    #return sorted list
 
+N = np.arange(10,100000,1000)
+execution_time_bubble = []
+for n in range(len(N)):
+    A = [p(randint(-100,100)) for i in range(0,n)] #list of random numbers between -100, 100, converted to our high precision data type
 
-A = [p(randint(-100,100)) for i in range(0,5)] #list of random numbers between -100, 100, converted to our high precision data type
+    tic = time.time()
+    out = bubble_sort(A)
+    toc = time.time()
 
-out = bubble_sort(A)                        #sort da list
-print([str(out[i]) for i in range(len(A))]) #print out string representations
+    execution_time_bubble.append(toc-tic)
 
 def merge(left :list , right:list) -> list: 
     merged_list = [] 
@@ -60,3 +68,26 @@ def merge_sort(arr: list) -> list:
     right = merge_sort(right)
 
     return merge(left, right)
+
+execution_time_merge = []
+for n in range(len(N)):
+    A = [p(randint(-100,100)) for i in range(0,n)] #list of random numbers between -100, 100, converted to our high precision data type
+    
+    tic = time.time()
+    out = merge_sort(A)
+    toc = time.time()
+
+    execution_time_merge.append(toc-tic)
+
+plt.plot(N,execution_time_bubble,label="Bubble Sort")
+plt.plot(N,execution_time_merge,label="Merge Sort")
+plt.xlabel("Number of Elements in List")
+plt.ylabel("Execution Time")
+plt.title("Time Complexity Comparison Between Merge and Bubble Sort")
+plt.legend(loc = "upper left")
+#plt.xscale("log")
+#plt.yscale("log")
+plt.savefig("timecomplexity.png",dpi=300)
+
+#print([str(out[i]) for i in range(len(A))]) #print out string representations
+
